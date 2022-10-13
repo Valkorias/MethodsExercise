502fa4014b64c95e3e0d2fb3c32f360384443fae
David Figone <david.louis.figone@gmail.com>
2022-10-13 14:22:36 -0500

    Even more

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,22 +4,22 @@
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("What is your name?");
-            // string userName = Console.ReadLine();
+            //// Console.WriteLine("What is your name?");
+            //// string userName = Console.ReadLine();
 
 
-            // Console.WriteLine("How old are you?");
-            // string age = Console.ReadLine();
+            //// Console.WriteLine("How old are you?");
+            //// string age = Console.ReadLine();
 
-            // Console.WriteLine("What is a name for an dog?");
-            // string userDogName = Console.ReadLine();
+            //// Console.WriteLine("What is a name for an dog?");
+            //// string userDogName = Console.ReadLine();
 
-            // Console.WriteLine("What color is your dog?");
-            // string userDogColor = Console.ReadLine();
+            //// Console.WriteLine("What color is your dog?");
+            //// string userDogColor = Console.ReadLine();
 
-            // Console.WriteLine($"There was a wanderer named {userName}, He was {age} years old.");
-            // Console.WriteLine($"He had a fearsome {userDogColor} dog named {userDogName}");
-            // Console.WriteLine($"{userName} loved {userDogName} but being {age} years old sure made life difficult!");
+            //// Console.WriteLine($"There was a wanderer named {userName}, He was {age} years old.");
+            //// Console.WriteLine($"He had a fearsome {userDogColor} dog named {userDogName}");
+            //// Console.WriteLine($"{userName} loved {userDogName} but being {age} years old sure made life difficult!");
 
             Console.WriteLine("Give me a number to add");
             int num1 = int.Parse(Console.ReadLine());
