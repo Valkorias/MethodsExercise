e02ee18398f8fc70108a46d35b1d31ae941e71a7
David Figone <david.louis.figone@gmail.com>
2022-10-13 14:02:34 -0500

    initial commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+
+
+            Console.WriteLine("How old are you?");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("What is a name for an dog?");
+            string userDogName = Console.ReadLine();
+
+            Console.WriteLine("What color is your dog?");
+            string userDogColor = Console.ReadLine();
+
+            Console.WriteLine($"There was a man named {userName}, He was {age} years old.");
+            Console.WriteLine($"He had a {userDogColor} dog named {userDogName}");
+            Console.WriteLine($"{userName} loved {userDogName} but he did not love being {age} years old!");
         }
     }
 }
